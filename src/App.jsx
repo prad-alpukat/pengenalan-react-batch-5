5968e95da52387b786582fb75971a4b24d158ca3
praditya <pradityaaldi.s@gmail.com>
2024-01-02 18:53:46 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar"
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>hello world</h1>
+      <h2>hello world</h2>
+    </>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <header>
+            <h1>Ini navbar</h1>
+        </header>
+    )
+}
